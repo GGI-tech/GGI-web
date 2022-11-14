4b8d7530c92a9b6ecd035dcfad83a01799f280e8
ShivamBunge <shivambunge9898@gmail.com>
2022-11-14 13:34:59 +0530

    blogs working

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
               <button class="inline-flex items-center rounded font-semibold">Explore</button>
               <ul class="absolute hidden pt-1 primary font-medium group-hover:block">
                 <li class=""><a class="whitespace-no-wrap block rounded-t bg-gray-100 py-2 px-4 hover:bg-gray-400" target="_blank" href="https://www.schrodingerbyggi.org/">Free Series</a></li>
-                <li class=""><a class="whitespace-no-wrap block bg-gray-100 py-2 px-6 hover:bg-gray-400" href="#">Blogs & Research</a></li>
+                <li class=""><a class="whitespace-no-wrap block bg-gray-100 py-2 px-6 hover:bg-gray-400" href="/blog">Blogs & Research</a></li>
                 <li class=""><a class="whitespace-no-wrap block rounded-b bg-gray-100 py-2 px-6 hover:bg-gray-400" href="/careers">Careers</a></li>
               </ul>
             </span>
