798779b47dfdc58859b9e70caec1b9d22309398c
ShivamBunge <shivambunge9898@gmail.com>
2022-12-02 13:34:48 +0530

    underlines added

--- components/global/Heading.jsx
+++ components/global/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Heading = (props) => {
     return (
         <div id='' className='flex justify-center mb-12 mt-48 md:mt-72'>
-            <h1 className='primary md:w-2/3 text-center lg:text-3xl md:text-2xl text-xl'>{props.children}</h1>
+            <h1 className='primary p-2 md:w-2/3 text-center text-3xl leading-relaxed'>{props.children}</h1>
         </div>
     );
 };
--- components/global/MainHero.jsx
+++ components/global/MainHero.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 const MainHero = () => {
     return (
-        <div className='hero text-white flex-col md:pt-24 md:px-24 h-screen '>
-            <div className='r1 flex gap-5 items-end flex-wrap'>
-                <h1 className='text-9xl font-black'>100X</h1>
-                <h1 className='text-2xl font-black'> YOUR CHANCES</h1>
-                <h1><button className='bg-white text-black px-4 py-2 hover:bg-black hover:text-white font-black rounded-full text-4xl'>TO GET IN</button></h1>
+        <div className='hero  text-white flex-col pt-36 md:pt-24 px-2 md:px-24 h-full'>
+            <div className='r1 flex gap-5 justify-center md:justify-start items-end flex-wrap'>
+                <h1 className='text-6xl md:text-9xl font-black arial'>100X</h1>
+                <h1 className='text-2xl font-black arial'> YOUR CHANCES</h1>
+                <h1><button className='bg-white text-black px-4 py-2 hover:bg-black hover:text-white font-black rounded-full text-3xl'>TO GET IN</button></h1>
             </div>
-            <div className='r2 flex justify-between mt-3 flex-wrap'>
-                <h1 className='text-5xl font-black max-w-xs'>MANAGEMENT CONSULTING</h1>
-                <h1 className='text-5xl font-black max-w-xs'>PUBLIC POLICY</h1>
+            <div className='r2 flex justify-between mt-4 flex-wrap'>
+                <h1 className='text-3xl font-black max-w-xs'>MANAGEMENT CONSULTING</h1>
+                <h1 className='text-3xl font-black max-w-xs'>PUBLIC POLICY</h1>
             </div>
-            <div className='r3 flex justify-center'>
+            <div className='r3 flex justify-center flex-wrap lg:flex-nowrap'>
                 <div className=''>
-                    <div className='flex mt-2 justify-center'><h1 className='text-5xl font-black max-w-xs'>PRODUCT MANAGEMENT</h1></div>
-                    <div className='r11 mt-4 '>
+                    <div className='flex mt-4 justify-center'><h1 className='text-3xl font-black max-w-xs'>PRODUCT MANAGEMENT</h1></div>
+                    <div className='r11 mt-4'>
                         <h1>We’re a social impact global organization with a mission to provide quality <br />Business & Policy Education for an Inclusive world.</h1>
                     </div>
                     <div className='r12 mt-5 grid grid-cols-2 md:grid-cols-4'>
@@ -38,7 +38,7 @@ const MainHero = () => {
                     </div>
                 </div>
                 <div>
-                    <Image src='/ggi.webp' alt="ggi" height={300} width={300} className="rounded-full" />
+                    <Image src='/ggi.webp' alt="ggi" height={250} width={250} className="rounded-full" />
                 </div>
 
             </div>
