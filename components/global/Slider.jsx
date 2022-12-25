4a66d902566b7132623a2af6a6c4d6c897d903be
ShivamBunge <shivambunge9898@gmail.com>
2022-12-25 23:07:25 +0530

    fixes

--- components/global/Slider.jsx
+++ components/global/Slider.jsx
@@ -56,7 +56,7 @@ const Slider = ({ slides }) => {
                   </div>
                   <div className='flex flex-col justify-center p-4 lg:w-1/2 text-sm'>
                     <p className='mb-12' dangerouslySetInnerHTML={{ __html: slide.review }}></p>
-                    {slide.link &&<a className="italic underline text-center text-blue-900" href={slide.link} target="_blank" >Go to Link</a>}
+                    {slide.link &&<a className="italic underline text-center text-blue-900" href={slide.link} target="_blank" rel="noreferrer">Go to Link</a>}
                   </div>
 
                 </div>
