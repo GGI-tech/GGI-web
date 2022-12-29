aad8deac0c4ba82648de0b6a5ca89e0de3b9f05d
ShivamBunge <shivambunge9898@gmail.com>
2022-12-29 19:56:42 +0530

    fixes

--- components/global/Curriculum.jsx
+++ components/global/Curriculum.jsx
@@ -42,7 +42,7 @@ const Curriculum = ({ data }) => {
 
         //     </div>
         // </div>
-        <div className='flex flex-wrap w-5/6  xl:w-2/3 mx-auto gap-5 md:gap-10 justify-center md:py-5'>
+        <div className='flex flex-wrap lg:w-5/6 xl:w-2/3 mx-auto gap-5 md:gap-10 justify-center '>
             {data.map((e,i) => {
                 return (
                     <div key={i} className='w-44 sm:w-48 text-center'>
