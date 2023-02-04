e80a6ac9c645a8c6aeca18fd53ca2e9f517927bf
ShivamBunge <shivambunge9898@gmail.com>
2023-02-04 17:57:43 +0530

    ga fixed

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,14 @@ import '../styles/globals.css';
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
+  
 
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <GoogleAnalytics gaMeasurementId={`${process.env.GOOGLE_ANALYTICS}`} trackPageViews={{ ignoreHashChange: true }} />     
+      <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews={{ ignoreHashChange: true }} />     
 
       <Navbar />
       
