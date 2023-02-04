4cc7ec353275795d65593053261f15e322b6f5d0
ShivamBunge <shivambunge9898@gmail.com>
2023-02-04 16:44:33 +0530

    google ana;ytics active

--- pages/_app.js
+++ pages/_app.js
@@ -2,50 +2,16 @@ import Head from 'next/head';
 import Navbar from '../components/global/Navbar';
 import '../styles/globals.css';
 
-// import Script from 'next/script'
-// import { useRouter } from 'next/router';
-// import { useEffect } from "react";
-// import * as gtag from "../lib/gtag"
-
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  // const ga = `https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`;
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const handleRouteChange = (url) => {
-  //     gtag.pageview(url);
-  //   };
-
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
 
   return (
     <>
-      {/* <Script strategy="afterInteractive" src={ga}></Script>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      /> */}
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <GoogleAnalytics gaMeasurementId={process.env.GOOGLE_ANALYTICS} trackPageViews={{ ignoreHashChange: true }} />     
+      <GoogleAnalytics gaMeasurementId={`${process.env.GOOGLE_ANALYTICS}`} trackPageViews={{ ignoreHashChange: true }} />     
 
       <Navbar />
       
