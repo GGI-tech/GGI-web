c086ec315aa75a01f132514fcaf081b20e63a042
ShivamBunge <shivambunge9898@gmail.com>
2023-02-16 14:21:16 +0530

    medical scholars pg

---
+++ pages/medical-scholars.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MedicalScholars from '../components/pages/MedicalScholars'
+import Head from 'next/head'
+const medicalscholars = () => {
+  return (
+    <>
+      <Head>
+        <title>MBA Scholars by Global Governance Initiative | Learn Business Live  &amp; Get Hired.</title>
+
+      </Head>
+      <MedicalScholars />
+    </>
+  )
+}
+
+export default medicalscholars
