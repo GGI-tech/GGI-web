04e94e8cd3225db2f61a4f0107e718305de9d44f
ShivamBunge <shivambunge9898@gmail.com>
2023-03-07 20:35:52 +0530

    emp img

--- components/global/Hero.jsx
+++ components/global/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = ({ image, heading, message }) => {
     <>
       <div className={`relative ${secondarynavbar ? 'mt-36' : 'mt-24'} `}>
       
-        <img src={image} class="w-full"/>
-          <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
-            {heading &&<h1 class="bg-black/30 font-serif text-2xl md:text-4xl p-2 md:py-12 md:px-36 border-2" dangerouslySetInnerHTML={{ __html: heading }}></h1>}
+        <img src={image} className="w-full"/>
+          <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
+            {heading &&<h1 className="bg-black/30 font-serif text-2xl md:text-4xl p-2 md:py-12 md:px-36 border-2" dangerouslySetInnerHTML={{ __html: heading }}></h1>}
             {message &&<p className='text-center mt-[2rem] text-xl font-serif' dangerouslySetInnerHTML={{ __html: message }}></p>}
 
           </div>
