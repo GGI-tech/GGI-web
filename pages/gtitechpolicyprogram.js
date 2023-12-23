703ba28efa47e6c99d2fee30e685fbc47ceefb58
nabilmullick <nabilmullick2@gmail.com>
2023-12-23 23:40:15 +0530

    Add GTI scholars program demo page

---
+++ pages/gtitechpolicyprogram.js
@@ -0,0 +1,18 @@
+import GTIPolicy from "../components/pages/GTIPolicy"
+import Head from "next/head"
+
+const ggipolicyscholarsprogram = () => {
+
+  return (
+    <>
+      <Head>
+        
+      
+      </Head>
+      <GTIPolicy />
+    </>
+
+  )
+}
+
+export default ggipolicyscholarsprogram
