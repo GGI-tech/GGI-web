d42ea0ec758e14d07dc5988f164d4aefade97ba7
nabilmullick <nabilmullick2@gmail.com>
2023-12-30 00:05:18 +0530

    Add SEO to GGI tech policy page

--- pages/ggitechpolicy.js
+++ pages/ggitechpolicy.js
@@ -6,16 +6,16 @@ const ggitechpolicy = () => {
   return (
     <>
       <Head>
-        <title>Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired.</title>
-        <meta name='description' content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
+        <title>Tech Policy Scholars by Global Governance Initiative | Learn Tech Policy Skills Live &amp; Get Hired.</title>
+        <meta name='description' content="Learn Tech Policy Skills Live, Get Hired, &amp; Network at GGI Tech Policy Scholar To Create an Impact in Society &nbsp;" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel='icon' href='/favicon.jpg' />
         <link rel="shortcut icon" href="https://static.wixstatic.com/media/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png" type="image/png" />
         <link rel="apple-touch-icon" href="https://static.wixstatic.com/media/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png" type="image/png" />
         <link rel="canonical" href="https://www.globalgovernanceinitiative.org/ggipolicyscholarsprogram" />
 
-        <meta property="og:title" content="Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired." />
-        <meta property="og:description" content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
+        <meta property="og:title" content="Tech Policy Scholars by Global Governance Initiative | Learn Tech Policy Skills Live &amp; Get Hired." />
+        <meta property="og:description" content="Learn Tech Policy Skills Live, Get Hired, &amp; Network at GGI Tech Policy Scholar To Create an Impact in Society &nbsp;" />
         <meta property="og:image" content="https://static.wixstatic.com/media/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png/v1/fit/w_2500,h_1330,al_c/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png" />
         <meta property="og:image:width" content="2500" />
         <meta property="og:image:height" content="1330" />
@@ -24,7 +24,7 @@ const ggitechpolicy = () => {
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired. " />
-        <meta name="twitter:description" content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
+        <meta name="twitter:description" content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Tech Policy Scholar To Create an Impact in Society &nbsp;" />
         <meta name="twitter:image" content="https://static.wixstatic.com/media/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png/v1/fit/w_2500,h_1330,al_c/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png" />
       
       </Head>
