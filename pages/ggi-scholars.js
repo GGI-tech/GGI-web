65c6cc90b86338f09356094b97428332b2069a35
nabilmullick <nabilmullick2@gmail.com>
2024-04-18 21:02:30 +0530

    Updates

--- pages/ggi-scholars.js
+++ pages/ggi-scholars.js
@@ -6,7 +6,7 @@ const ggischolars = () => {
   return (
     <>
       <Head>
-        <title>MBA Scholars by Global Governance Initiative | Learn Business Live  &amp; Get Hired.</title>
+        <title>Impact Scholar by Global Governance Initiative | Learn Business Live  &amp; Get Hired.</title>
         <meta property="description" content="Learn Business Skills Live, Get Hired, &amp; Network at GGI MBA Scholar To Become an Impactful Business Leader&nbsp;" />
 
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
