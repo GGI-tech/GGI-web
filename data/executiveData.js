b22311d2208ae39fba0bf493a2821f6a77c35217
nabilmullick <nabilmullick2@gmail.com>
2024-08-31 16:01:30 +0530

    Add Executive Management page

---
+++ data/executiveData.js
@@ -0,0 +1,59 @@
+export const stages = [
+    {
+        "id": 0,
+        "ques": "Stage 1: Foundational Upskilling",
+        "ans": [`<p class='mb-4'>We believe that strong foundations are the key to long-term success. In this initial stage, you’ll gain access to our exclusive “MBA in 10 Minutes” series, delivered by an Ex-BCG Consultant.<br/><br/>These concise, high-impact videos will walk you through core competencies in Finance, Statistics, Marketing, Branding, and Operations. These foundational skills will be rigorously tested, preparing you to tackle the most challenging interviews in consulting, policy, or business.</p>`]
+    },
+    {
+        "id":1,
+        "ques":"Stage 2: Advanced Strategy",
+        "ans":[`Building on your foundational skills, the advanced strategy stage ensures you’re prepared to meet the demands of high-stakes consulting interviews and leadership roles. You’ll gain exclusive access to the Live weekly deep dive sessions with GGI Cofounder where you’d interactively learn how to problem solve world’s and India's most pressing contemporary issues.<br/><br/>This will be accompanied with Trinity Learning Experience, led by McKinsey Partners, where you’ll learn to apply these skills at a strategic level. You’ll also benefit from recorded masterclasses by industry giants, including the former CEO of McKinsey & Company, who will provide you with a macro-level perspective of the industry. These insights could be critical when facing interviews with top consulting firms and industry leaders.  `]
+
+    },
+    {
+        "id":2,
+        "ques":"Stage 3: Executive Startup",
+        "ans":[`To truly excel in consulting, policy, or product management, you need more than theoretical knowledge—you need practical experience. Our Startup weekend will further push you to bring out your entrepreneurship skills. This unique experience allows you to apply the concepts from Stages 1 and 2 in a practical, high-stakes setting. Many GGI Alumni have leveraged this experience to secure senior roles and starting ventures in consulting and policy.`]
+
+    },
+    {
+        "id":3,
+        "ques":"Stage 4: Peer Collaboration",
+        "ans":[`At the heart of the Global Governance Initiative Executive program is our vibrant community. Throughout the program, you’ll have the opportunity to collaborate with peers—your ‘buddies’—to share best practices, discuss challenges from your deep dive sessions, AI Case-Based Internship, and build lasting professional relationships. Our community extends beyond the program, with social media groups and networking events providing continuous opportunities for collaboration and growth. `]
+    },
+    {
+        "id":4,
+        "ques": "Stage 5:  Executive Networking",
+        "ans":[`Stage 5: Executive Networking
+In leadership, your network is as crucial as your skills. Our Networking Nights offer you the chance to connect with other leaders, share successes, and seek advice during challenging times. These events are designed to help you build a supportive community that will be there for you long after the program ends.
+. 
+
+        ​`]
+    }
+]
+export const curriculum = [
+    {
+        "title": "3 Months",
+        "description": "Program length, flexible upon your needs and availability"
+    },
+    {
+        "title": "30+ Hours of Business Masterclasses",
+        "description": "Over 12 weeks, with availability of recordings for a time period"
+    },
+    {
+        "title": "60+ Hours of Business Interview Practice",
+        "description": "Via our inhouse AI Based software"
+    }, 
+    {
+        "title": "Unlimited Networking",
+        "description": "You continue to be a part of the family post your graduation"
+    },
+    {
+        "title": "Executive Startup Weekend",
+        "description": "Peer to Peer projects"
+    },
+    {
+        "title": "10+ Hours of Trinity",
+        "description": "You get access to recorded Masterclasses of top speakers in consulting at GGI"
+    }
+]
