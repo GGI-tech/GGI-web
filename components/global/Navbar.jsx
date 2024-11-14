ca7249cb3cf581fc3283bc9ed496b26f37b7b3c5
manyajsingh <139053850+manyajsingh@users.noreply.github.com>
2024-11-14 12:41:10 +0530

    pre to PRE

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = ({applicationbanner}) => {
               <button className="inline-flex items-center rounded font-semibold ">FOR COLLEGE STUDENTS</button>
               <ul className="absolute hidden pt-1 primary font-medium group-hover:block">
                 <Link href='/ggi-scholars'>
-                  <a target="_blank" className="whitespace-no-wrap block bg-gray-100 py-1 px-4 hover:bg-gray-400">Pre-MBA</a>
+                  <a target="_blank" className="whitespace-no-wrap block bg-gray-100 py-1 px-4 hover:bg-gray-400">PRE-MBA</a>
                 </Link>
                 <Link href='/impact-fellowship'>
                   <a target="_blank" className="whitespace-no-wrap block bg-gray-100 py-1 px-4 hover:bg-gray-400">FELLOWSHIP</a>
@@ -250,7 +250,7 @@ const Navbar = ({applicationbanner}) => {
               {dropdownCollege && (
                 <ul className="pt-1 bg-black text-white font-medium">
                   <Link prefetch={false} href='/ggi-scholars'>
-                    <a className="block bg-gray-700 py-1 px-4 hover:bg-gray-800">Pre-MBA</a>
+                    <a className="block bg-gray-700 py-1 px-4 hover:bg-gray-800">PRE-MBA</a>
                   </Link>
                   <Link prefetch={false} href='/impact-fellowship'>
                     <a className="block bg-gray-700 py-1 px-4 hover:bg-gray-800">FELLOWSHIP</a>
