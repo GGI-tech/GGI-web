7786a43b2cacc35117aae911a1585c51e351b747
manyajsingh <139053850+manyajsingh@users.noreply.github.com>
2025-04-27 13:09:23 +0530

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,41 @@
+// import Head from 'next/head';
+// import Navbar from '../components/global/Navbar';
+// import '../styles/globals.css';
+// import { useRouter } from 'next/router';
+// import { GoogleAnalytics } from "nextjs-google-analytics";
+
+// function MyApp({ Component, pageProps }) {
+
+//   const router = useRouter();
+//   const showHeader = router.pathname.includes('30-under-30') ? false : true;
+
+//   return (
+//     <>
+//       <Head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+//         <script> 
+//           {`
+//           window.dataLayer = window.dataLayer || []; 
+//           function gtag(){
+//              dataLayer.push(arguments);
+//           } 
+//           gtag('js', new Date());
+//           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}'); 
+//           `}
+//         </script>
+//       </Head>
+//       <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews={{ ignoreHashChange: true }} />
+//       <Navbar applicationbanner={showHeader}/>
+
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
+// export default MyApp;
+
+
 import Head from 'next/head';
 import Navbar from '../components/global/Navbar';
 import '../styles/globals.css';
@@ -5,7 +43,6 @@ import { useRouter } from 'next/router';
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-
   const router = useRouter();
   const showHeader = router.pathname.includes('30-under-30') ? false : true;
 
@@ -13,21 +50,21 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11172189274"></script>
-        <script> 
-          {`
-          window.dataLayer = window.dataLayer || []; 
-          function gtag(){
-             dataLayer.push(arguments);
-          } 
-          gtag('js', new Date());
-          gtag('config', 'AW-11172189274'); 
-          `}
-        </script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `
+          }}
+        />
       </Head>
-      <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews={{ ignoreHashChange: true }} />
-      <Navbar applicationbanner={showHeader}/>
 
+      <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews={{ ignoreHashChange: true }} />
+      <Navbar applicationbanner={showHeader} />
       <Component {...pageProps} />
     </>
   );
