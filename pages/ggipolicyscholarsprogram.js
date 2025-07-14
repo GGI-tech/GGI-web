5751a4b5639ba41e400b1838fa64e4c38f5c0a09
adityakr26 <106471773+adityakr26@users.noreply.github.com>
2025-07-14 18:04:27 +0530

    Update ggipolicyscholarsprogram.js

--- pages/ggipolicyscholarsprogram.js
+++ pages/ggipolicyscholarsprogram.js
@@ -6,15 +6,15 @@ const ggipolicyscholarsprogram = () => {
   return (
     <>
       <Head>
-        <title>Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired.</title>
-        <meta name='description' content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
+        <title>Policy Consulting Lab by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired.</title>
+        <meta name='description' content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Consulting Lab To Create an Impact in Society &nbsp;" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel='icon' href='/favicon.jpg' />
         <link rel="shortcut icon" href="https://static.wixstatic.com/media/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png" type="image/png" />
         <link rel="apple-touch-icon" href="https://static.wixstatic.com/media/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/65ca63_b536077bdc99495ca651850cdaca0c92%7Emv2.png" type="image/png" />
         <link rel="canonical" href="https://www.globalgovernanceinitiative.org/ggipolicyscholarsprogram" />
 
-        <meta property="og:title" content="Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired." />
+        <meta property="og:title" content="Policy Consulting Lab by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired." />
         <meta property="og:description" content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
         <meta property="og:image" content="https://static.wixstatic.com/media/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png/v1/fit/w_2500,h_1330,al_c/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png" />
         <meta property="og:image:width" content="2500" />
@@ -23,7 +23,7 @@ const ggipolicyscholarsprogram = () => {
         <meta property="og:site_name" content="GGI" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Policy Scholars by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired. " />
+        <meta name="twitter:title" content="Policy Consulting Lab by Global Governance Initiative | Learn Policy Skills Live &amp; Get Hired. " />
         <meta name="twitter:description" content="Learn Policy Skills Live, Get Hired, &amp; Network at GGI Policy Scholar To Create an Impact in Society &nbsp;" />
         <meta name="twitter:image" content="https://static.wixstatic.com/media/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png/v1/fit/w_2500,h_1330,al_c/f7385c_30f5f1a667d24a7c8112b4a03168e15e%7Emv2.png" />
       
