45d3f4b6030ce210b47e0bfeb068ebdae418a32e
manyajsingh <139053850+manyajsingh@users.noreply.github.com>
2025-12-20 01:45:34 +0530

    mba to pgp

--- components/global/Accordian.jsx
+++ components/global/Accordian.jsx
@@ -8,7 +8,7 @@ const FAQ = [
         <br><br>      
         A. You can access free recorded MBA in 10 minutes series <u><a href="https://thetrinitybyggi.org/" target="_blank" rel="noreferrer noopener">here</a></u> on the Trinity which will allow you to directly learn MBA lessons in 10 minutes from an Ex BCG consultant on Strategy/ Finance/ Operations etc. free of cost.&nbsp;                
         <br><br>        
-        B.&nbsp;</span><u><a href="/ggi-scholars" target="_blank" rel="noreferrer noopener">GGI Management Scholars</a></u>- Serves as a classic <span style="font-weight:bold;">Alt MBA &amp; Pre MBA,</span> designed specifically for those who wish to pivot their career into management consulting, business strategy, and product management        
+        B.&nbsp;</span><u><a href="/ggi-scholars" target="_blank" rel="noreferrer noopener">GGI PGP Scholars</a></u>- Serves as a classic <span style="font-weight:bold;">Alt MBA &amp; Pre MBA,</span> designed specifically for those who wish to pivot their career into management consulting, business strategy, and product management        
         <br><br>        
         <p >C.&nbsp;<u><a href="/impact-fellowship" target="_blank" rel="noreferrer noopener">GGI Impact Fellowship</a></u>-&nbsp; Serves as a classic experiential learning and work-study model, designed specifically for those who are exploring Plan B for Civil Services and looking to pivot their career into <span style="font-weight:bold;">management/impact consulting </span>and policy as you work on projects with mentors from MBB
         <br><br>        
@@ -31,7 +31,7 @@ const FAQ = [
     },
     {
         "id":4,
-        "ques":"Am I eligible for the GGI Management, Policy or Impact Fellowship Program?",
+        "ques":"Am I eligible for the GGI PGP or Impact Fellowship Program?",
         "ans":`The pre-requisites of Global Governance Initiative Programs are as below :<br><br>
          1. <u>Interested in pursuing a career</u> in Management Consulting, Public Policy or Product Management.<br><br>
          2. <u>Global Governance is a mission-oriented organization</u> with an aim to empower professionals and women to have a seat on the table in business and policy roles. <br><br>
